0a8f5260459a99250327a50bb8fcf12dddd9df9a
phuong pham <phuong.pham@thk.vn>
2023-08-07 00:23:58 +0700

    update api cancel

--- Payment/Utilities/Constants.cs
+++ Payment/Utilities/Constants.cs
@@ -4,5 +4,6 @@
     {
         public const string URL_API_LOGIN = @"https://api.fortunapay.com.br/login";
         public const string URL_API_STATUS_PAYMENT = @"https://api.fortunapay.com.br/status/";
+        public const string URL_API_CANCEL = @"https://api.fortunapay.com.br/cancel";
     }
 }
--- Payment/Views/OrderStatusView.xaml.cs
+++ Payment/Views/OrderStatusView.xaml.cs
@@ -1,17 +1,12 @@
-﻿using Payment.ViewModels;
+﻿using Payment.Models;
+using Payment.ViewModels;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Payment.Views
 {
@@ -45,9 +40,53 @@ namespace Payment.Views
             if (this.DataContext != null)
             {
                 OrderStatusViewModel vm = (OrderStatusViewModel)this.DataContext;
-                if(vm.SelectedParameter!=null && vm.SelectedParameter.OrderType==Utilities.TypeOrder.Waiting)
+                if (vm.SelectedParameter != null && vm.SelectedParameter.OrderType == Utilities.TypeOrder.Waiting)
                 {
                     vm.mainViewModel.SelectedViewModel = new QrCodeViewModel(vm.mainViewModel, vm.SelectedParameter);
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.DataContext != null)
+            {
+                OrderStatusViewModel vm = (OrderStatusViewModel)this.DataContext;
+                if (vm.SelectedParameter != null && vm.SelectedParameter.OrderType == Utilities.TypeOrder.Waiting)
+                {
+                    //call api remove order
+                    bool isSuccessStatusCode = false;
+                    ResponseLoginModel result = new ResponseLoginModel();
+                    HttpMessageHandler handler = new HttpClientHandler();
+
+                    var httpClient = new HttpClient(handler)
+                    {
+                        BaseAddress = new Uri(Utilities.Constants.URL_API_CANCEL),
+                        Timeout = new TimeSpan(0, 2, 0)
+                    };
+                    httpClient.DefaultRequestHeaders.Authorization
+                         = new AuthenticationHeaderValue("Bearer", vm.mainViewModel.ResponseLoginModel.access_token);
+
+                    httpClient.DefaultRequestHeaders.Add("ContentType", "application/json");
+
+                    var stringContent = new FormUrlEncodedContent(new[]
+                    {
+                        new KeyValuePair<string, string>("transaction_id", vm.SelectedParameter.OrderNumber),
+                    });
+                    HttpResponseMessage response = httpClient.PostAsync(Utilities.Constants.URL_API_CANCEL, stringContent).Result;
+                    isSuccessStatusCode = response.IsSuccessStatusCode;
+
+                    if (isSuccessStatusCode)
+                    {
+                        //var data = response.Content.ReadAsStringAsync().Result;
+                        //_viewModel.mainViewModel.ResponseLoginModel = JsonSerializer.Deserialize<ResponseLoginModel>(data);
+                        vm.OrderList.Remove(vm.SelectedParameter);
+                        vm.mainViewModel.OrderList.Remove(vm.SelectedParameter);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cancel fail Fail");
+                    }
                 }
             }
         }
